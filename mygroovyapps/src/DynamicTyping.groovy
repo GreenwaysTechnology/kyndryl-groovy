1e0425ed074c20efe2e16bee84b35cb94fcbb1d5
Subramanian Murugan <sasubramanian_md@hotmail.com>
2022-09-14 20:56:34 +0530

    groovy fundamentals

---
+++ mygroovyapps/src/DynamicTyping.groovy
@@ -0,0 +1,10 @@
+def myvar = 100
+println "MyVar with Number" + myvar
+
+//reinitalize the variable with different values
+myvar = true
+println "MyVar with Boolean" + myvar
+
+myvar = 89.89
+println "MyVar with floating " + myvar
+
---
+++ mygroovyapps/src/StrongTyping.groovy
@@ -0,0 +1,25 @@
+//strong type
+//integer
+byte b = 100
+short s = 900
+int i = 90
+long lng = 98888
+println "Strong Prmitive Types"
+println "Byte " + b
+println "Short " + s
+println "Integer " + i
+println "Long " + lng
+
+//floating points
+
+float f = 100.78
+double d =987.78
+
+println "Float " + f
+println "Double " + d
+
+//characters
+char c ='a'
+char c1 = 98
+println "Character 1" + c
+println "Character 2 " + c1
